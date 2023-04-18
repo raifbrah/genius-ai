439910af6211c78ecfacadbdd7d53955b3dd7577
raifbrah <raifbrah@gmail.com>
2023-04-19 00:04:23 +0300

    Update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    baseURL: "https://raifbrah.github.io/genius-ai/",
+    // cdnURL: "https://raifbrah.github.io/genius-ai/",
+    baseURL: "/genius-ai/",
     head: {
       title: "Genius AI",
     },
   },
-  modules: ["@kevinmarrec/nuxt-pwa"],
-  pwa: {
-    manifest: {
-      name: "Genius AI",
-      short_name: "Genius AI",
-      lang: "ru",
-      description: "Fast AI Chat",
-    },
-  },
+  // modules: ["@kevinmarrec/nuxt-pwa"],
+  // pwa: {
+  //   manifest: {
+  //     name: "Genius AI",
+  //     short_name: "Genius AI",
+  //     lang: "ru",
+  //     description: "Fast AI Chat",
+  //   },
+  // },
 });

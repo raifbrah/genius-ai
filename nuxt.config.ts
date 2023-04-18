a91fca7936a0a85a98249b8e010a8230a4861bec
raifbrah <raifbrah@gmail.com>
2023-04-18 21:54:33 +0300

    Update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   app: {
+    baseURL: "/genius-ai/",
     head: {
       title: "Genius AI",
     },

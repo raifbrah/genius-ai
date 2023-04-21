3b97a4e71e379577993b6294d65456d868f04a5d
raifbrah <raifbrah@gmail.com>
2023-04-22 00:00:24 +0300

    Added PWA

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  // nitro: {
-  //   preset: "github-pages",
-  // },
-  // ssr: false,
   app: {
-    // baseURL: "/genius-ai/",
-    // head: {
-    //   title: "Genius AI",
-    // },
+    head: {
+      title: "Genius AI",
+    },
   },
-  // modules: ["@kevinmarrec/nuxt-pwa"],
-  // pwa: {
-  //   manifest: {
-  //     name: "Genius AI",
-  //     short_name: "Genius AI",
-  //     lang: "ru",
-  //     description: "Fast AI Chat",
-  //   },
-  // },
+  modules: ["@kevinmarrec/nuxt-pwa"],
+  pwa: {
+    manifest: {
+      name: "Genius AI",
+      short_name: "Genius AI",
+      lang: "ru",
+      description: "Fast AI Chat",
+    },
+  },
 });
